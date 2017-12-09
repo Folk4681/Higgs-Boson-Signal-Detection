679eb6d1492c35cf84b6a43f402a9b1b65a799ac
Vighnesh Souda <vsouda@users.noreply.github.com>
2017-12-09 03:47:15 -0500

    Random sample 0.2% of entire Higgs Boson data, save to file

--- src/main/scala/dataVisualizer.scala
+++ src/main/scala/dataVisualizer.scala
@@ -1,3 +1,38 @@
+import org.apache.log4j.{Level, LogManager}
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+
+
 object dataVisualizer {
 
+  def main(args: Array[String]): Unit = {
+    LogManager.getLogger("org").setLevel(Level.OFF)
+    LogManager.getLogger("akka").setLevel(Level.OFF)
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .master("local")
+      .getOrCreate()
+
+    import spark.implicits._
+    val df = spark.read.csv("data/HIGGS.csv")
+
+    val newDF = df.sample(false, 0.0002)
+
+    newDF.coalesce(1).write.option("header",true).csv("data/HIGGSsample")
+
+    spark.close()
+
+    val conf = new SparkConf().setAppName("dataProcessing").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+
+    import org.apache.hadoop.fs._
+    import org.apache.hadoop.fs.FileSystem
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+    val file = fs.globStatus(new Path("data/HIGGSsample/part*"))(0).getPath().getName()
+
+    fs.rename(new Path("data/HIGGSsample/" + file), new Path("data/HIGGSsample.csv"))
+    fs.delete(new Path("mydata.csv-temp"), true)
+  }
 }
